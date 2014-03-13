e24c869c87587deb4714d41000130c5a905c2911
Charu <charu.gangal@vvdntech.com>
2014-03-13 18:14:12 +0530

    reverting reports.py

--- deviceapp/meru_device/device_app/reports.py
+++ deviceapp/meru_device/device_app/reports.py
@@ -25,7 +25,7 @@ class ClientReport():
         self.mac = kwargs['mac']
         self.doc_list = []
         for mac in self.mac:
-            self.cursor = DB.devices.find({ "timestamp": {"$gt": self.gt, "$lt": self.lt}}).\
+            self.cursor = DB.devices.find({ "lower_snum":mac.lower(), "timestamp": {"$gt": self.gt, "$lt": self.lt}}).\
                                 sort('timestamp', -1)
             for doc in self.cursor:
                 self.doc_list.append(doc)

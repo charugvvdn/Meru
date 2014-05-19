4584b41952c3b8046742cedec22a37cc891d7247
Charu <charu.gangal@vvdntech.com>
2014-05-19 10:49:28 +0530

    mongo db data updation

--- deviceapp/meru_device/device_app/views.py
+++ deviceapp/meru_device/device_app/views.py
@@ -213,7 +213,7 @@ class Raw_Model():
         :param mac:
         """
 
-        cursor = connections['meru_cnms_sitegroup_dev'].cursor()
+        cursor = connections['meru_cnms_sitegroup'].cursor()
 
 
         command_query = """SELECT command_json, command_id FROM meru_command WHERE \
@@ -253,7 +253,7 @@ class Raw_Model():
 	try:
 		query = """ UPDATE meru_command SET command_status = 2 WHERE \
 				command_id = '%s'""" % command_id
-		cursor = connections['meru_cnms_sitegroup_dev'].cursor()
+		cursor = connections['meru_cnms_sitegroup'].cursor()
 		cursor.execute(query)
 		cursor.close()
 	except Exception as error:
@@ -295,7 +295,7 @@ class DeviceApplication(View):
         `device_mac` = '%s'" % mac
 	print "mysql access in get"
 	print datetime.datetime.now()
-        cursor = connections['meru_cnms_sitegroup_dev'].cursor()
+        cursor = connections['meru_cnms_sitegroup'].cursor()
         cursor.execute(query)
 	print "process complete"
 	print datetime.datetime.now()
@@ -344,7 +344,7 @@ class DeviceApplication(View):
 	print datetime.datetime.now()
         query = "SELECT COUNT(1) FROM meru_device WHERE \
         `device_mac` = '%s'" % mac
-        cursor = connections['meru_cnms_sitegroup_dev'].cursor()
+        cursor = connections['meru_cnms_sitegroup'].cursor()
         cursor.execute(query)
 	print "process complete"
 	print datetime.datetime.now()
@@ -445,14 +445,14 @@ class DeviceApplication(View):
                 self.false_response["mac"] = mac
                 query = "SELECT COUNT(1) FROM meru_device WHERE \
                 `device_mac` = '%s'" % mac
-                cursor = connections['meru_cnms_sitegroup_dev'].cursor()
+                cursor = connections['meru_cnms_sitegroup'].cursor()
                 cursor.execute(query)
                 result = cursor.fetchall()
                 if not result[0][0]:
                     return HttpResponse(json.dumps(self.false_response))
                 if put_data["status"].lower() == "true":
                     try:
-                        db = mydb.connect(host='localhost', user='root', db='meru_cnms_sitegroup_dev', passwd='root')
+                        db = mydb.connect(host='localhost', user='root', db='meru_cnms_sitegroup', passwd='root')
                         query = """ UPDATE meru_command SET command_status = 2 WHERE \
                                 command_mac = '%s'""" % mac
                         cursor = db.cursor()
